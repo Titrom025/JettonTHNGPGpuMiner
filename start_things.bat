2eb73a27fd4951aa6fa9e960af18250990d478ff
Roman Titkov <titkov01@mail.ru>
2024-06-24 14:47:52 +0300

    Set default givers to 200

--- start_things.bat
+++ start_things.bat
@@ -2,7 +2,7 @@
 call ".\npm_install.bat"
 
 :_minerstart
-node send_universal_things.js --api tonapi --givers 10000
+node send_universal_things.js --api tonapi --givers 200
 goto _minerstart
 
 pause
